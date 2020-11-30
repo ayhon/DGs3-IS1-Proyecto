e834f214fce7dec5eb9ed90e17876bf48aafb702
Jimmy Cai <jimmehcai@gmail.com>
2020-11-30 16:42:58 +0100

    Mejorar el estilo de pantalla de auth

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -20,6 +20,12 @@ import * as LocalAuthentication from 'expo-local-authentication';
 
 import { Credentials } from '../constants/DemoUser';
 
+import {
+    useFonts,
+    MajorMonoDisplay_400Regular,
+} from '@expo-google-fonts/major-mono-display';
+import { AppLoading } from "expo";
+
 const LoginForm = ({ navigation, setCurrentScreen, toggleSnackBar }) => {
     const [username, setUsername] = React.useState("");
     const [password, setPassword] = React.useState("");
@@ -76,11 +82,12 @@ const LoginForm = ({ navigation, setCurrentScreen, toggleSnackBar }) => {
                 mode="contained"
                 onPress={logIn}
                 style={{ marginVertical: 10 }}
+                contentStyle={{ paddingVertical: 2 }}
             >
                 Log in
             </Button>
 
-            <Button icon="creation" mode="outlined" onPress={register}>
+            <Button icon="creation" mode="outlined" onPress={register} contentStyle={{ paddingVertical: 2 }}>
                 Register
             </Button>
         </View>
@@ -118,13 +125,14 @@ const RegisterForm = ({ navigation, setCurrentScreen }) => {
             placeholder="Repeat password"
             autoCompleteType="password"
             secureTextEntry
+
         />
         <View style={{ display: "flex", flexDirection: "column" }}>
-            <Button icon="login" mode="contained" onPress={onRegister} style={{ marginVertical: 10 }}>
+            <Button icon="login" mode="contained" onPress={onRegister} style={{ marginVertical: 10 }} contentStyle={{ paddingVertical: 2 }}>
                 Register
             </Button>
 
-            <Button icon="account" mode="outlined" onPress={() => { setCurrentScreen("login") }}>
+            <Button icon="account" mode="outlined" onPress={() => { setCurrentScreen("login") }} contentStyle={{ paddingVertical: 2 }}>
                 I have an account
             </Button>
         </View>
@@ -137,51 +145,64 @@ export default function AuthScreen({ navigation }: any) {
     const onToggleSnackBar = () => setVisible(!visible);
     const onDismissSnackBar = () => setVisible(false);
 
-    return (
-        <>
-            <ImageBackground
-                source={{ uri: "https://i.imgur.com/6MMrZRV.png" }}
-                style={{
-                    width: 1648,
-                    height: 1165,
-                    flex: 1,
-                    justifyContent: "center",
-                    position: "absolute",
-                    opacity: 0.5
-                }}
-            >
-            </ImageBackground>
+    let [fontsLoaded] = useFonts({
+        MajorMonoDisplay_400Regular
+    });
 
-            <KeyboardAvoidingView
-                behavior={Platform.OS == "ios" ? "padding" : undefined}
-                style={styles.container}
-            >
-                <Image
-                    source={{ uri: "https://i.imgur.com/ibKSsXA.png" }}
-                    style={{ width: 60, height: 60 }}
-                />
-                <Title style={{
-                    ...styles.title
-                }}>Welcome to Scich.at</Title>
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    else {
+        return (
+            <>
+                <ImageBackground
+                    source={{ uri: "https://i.imgur.com/6MMrZRV.png" }}
+                    style={{
+                        width: 1648,
+                        height: 1165,
+                        flex: 1,
+                        justifyContent: "center",
+                        position: "absolute",
+                        opacity: 0.5
+                    }}
+                >
+                </ImageBackground>
 
-                <Surface style={{
-                    padding: 20,
-                    elevation: 1,
-                    borderRadius: 8
-                }}>
-                    {currentScreen == "login" ?
-                        <LoginForm navigation={navigation} setCurrentScreen={setCurrentScreen} toggleSnackBar={onToggleSnackBar} />
-                        :
-                        <RegisterForm navigation={navigation} setCurrentScreen={setCurrentScreen} />
-                    }
-                </Surface>
-            </KeyboardAvoidingView>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS == "ios" ? "padding" : undefined}
+                    style={styles.container}
+                >
+                    <Image
+                        source={{ uri: "https://i.imgur.com/ibKSsXA.png" }}
+                        style={{ width: 60, height: 60 }}
+                    />
+                    <Title style={{
+                        ...styles.title,
+                        fontFamily: 'MajorMonoDisplay_400Regular',
+                        lineHeight: 35
+                    }}>Welcome to {"\n"}
+                    scich.at</Title>
 
-            <Snackbar visible={visible} onDismiss={onDismissSnackBar}>
-                Wrong credentials
-            </Snackbar>
-        </>
-    );
+                    <Surface style={{
+                        padding: 20,
+                        elevation: 1,
+                        borderRadius: 8
+                    }}>
+                        {currentScreen == "login" ?
+                            <LoginForm navigation={navigation} setCurrentScreen={setCurrentScreen} toggleSnackBar={onToggleSnackBar} />
+                            :
+                            <RegisterForm navigation={navigation} setCurrentScreen={setCurrentScreen} />
+                        }
+                    </Surface>
+                </KeyboardAvoidingView>
+
+                <Snackbar visible={visible} onDismiss={onDismissSnackBar}>
+                    Wrong credentials
+                </Snackbar>
+            </>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
@@ -194,7 +215,6 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 24,
-        fontWeight: "bold",
         marginBottom: 10,
     },
     input: {
