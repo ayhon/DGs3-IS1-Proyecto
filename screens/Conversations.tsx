c0149f76b79bf140b38d24d0d98129ba32b1613b
Jimmy Cai <jimmehcai@gmail.com>
2020-11-30 13:34:52 +0100

    Añadir menu para cerrar sesión

--- screens/Conversations.tsx
+++ screens/Conversations.tsx
@@ -3,16 +3,35 @@ import { StyleSheet, View, FlatList } from "react-native";
 import ConversationCard from "../components/ConversationCard";
 import conversationData from "../assets/demo/DummyConversations";
 import Headbar from '../components/headbar.js'
+import { Appbar, Avatar, Menu } from "react-native-paper";
 
 export default function Conversations({ navigation }: any) {
+    const [visible, setVisible] = React.useState(false);
+
+    const openMenu = () => setVisible(true);
+    const closeMenu = () => setVisible(false);
+
     return (
         <>
-            <Headbar
-                hasBack={false}
-                back={() => alert("Adios")}
-                title="Conversations"
-                sub=""
-                puntos={() => alert("Mas opciones")} />
+            <Appbar.Header>
+                <Appbar.BackAction style={{ display: "none" }} />
+                <Avatar.Image
+                    size={36}
+                    source={{ uri: "https://i.imgur.com/ibKSsXA.png" }}
+                    style={{
+                        marginLeft: 10
+                    }}
+                />
+                <Appbar.Content title="Scich.at" />
+
+                <Menu
+                    visible={visible}
+                    onDismiss={closeMenu}
+                    anchor={<Appbar.Action icon="dots-vertical" onPress={openMenu} />}>
+                    <Menu.Item onPress={() => { navigation.navigate('AuthScreen') }} title="Log out" />
+                </Menu>
+
+            </Appbar.Header>
             <View style={styles.container}>
                 <FlatList
                     data={conversationData}
