0b71ce1edd3e73d2c0a3dc48ae17664d9f23d6d4
Jimmy Cai <jimmehcai@gmail.com>
2020-11-30 09:03:19 +0100

    Arreglar Warning de Conversations

--- screens/Conversations.tsx
+++ screens/Conversations.tsx
@@ -2,47 +2,49 @@ import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 import ConversationCard from "../components/ConversationCard";
 import conversationData from "../assets/demo/DummyConversations";
-import {DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import Headbar from '../components/headbar.js'
 
 // const accentColor = "#0039a2";
 
 const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#ddd56e',
-    accent: 'grey',
-  },
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#ddd56e',
+        accent: 'grey',
+    },
 };
 
 export default function Conversations({ navigation }: any) {
-  return (
-    <PaperProvider theme={theme}>
-      <Headbar 
-        hasBack={false}
-        back={()=>alert("Adios")} 
-        title="Conversations" 
-        sub="" 
-        puntos={()=>alert("Mas opciones")}/>
-      <View style={styles.container}>
-        <FlatList
-          data={conversationData}
-          renderItem={({ item }) => {
-            return <ConversationCard data={item} navigation={navigation} />;
-          }}
-        />
-      </View>
-    </PaperProvider>
-  );
+    return (
+        <PaperProvider theme={theme}>
+            <Headbar
+                hasBack={false}
+                back={() => alert("Adios")}
+                title="Conversations"
+                sub=""
+                puntos={() => alert("Mas opciones")} />
+            <View style={styles.container}>
+                <FlatList
+                    data={conversationData}
+                    renderItem={({ item }) => {
+                        return <ConversationCard data={item} navigation={navigation} />;
+                    }}
+                    keyExtractor={item => item.key.toString()}
+
+                />
+            </View>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ebebeb",
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ebebeb",
+    },
 });
